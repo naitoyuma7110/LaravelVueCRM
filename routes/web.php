e9d4deb8d2c1779a2feb436e5a920cc3a141cb08
naitoyuma3230 <naitoyuma@outlook.jp>
2023-01-12 17:19:35 +0900

    item関連の準備

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,10 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 use App\Http\Controllers\InertiaTestController;
+use App\Http\Controllers\ItemController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::resource('item',ItemController::class)->middleware(['auth', 'verified']);
+
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -52,5 +45,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 
 require __DIR__ . '/auth.php';
