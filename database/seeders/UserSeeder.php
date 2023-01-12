cf0b3267711e1253f26a0a6459a8f493169fe649
naitoyuma3230 <naitoyuma@outlook.jp>
2023-01-12 17:21:57 +0900

    ブランチ切れてるかテスト

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,6 +17,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        
         DB::table('users')->insert([
             'name' => 'test',
             'email' => 'test@test.com',
