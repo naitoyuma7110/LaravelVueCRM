fde25a455b8eccfa636f4eb56fcf61c25dfb153d
naitoyuma3230 <naitoyuma@outlook.jp>
2023-01-25 11:42:13 +0900

    Factoryで大量レコード挿入

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,14 +23,16 @@ class DatabaseSeeder extends Seeder
       UserSeeder::class,
       ItemSeeder::class
     ]);
+    // DatabaseSeederに直書き
     \App\Models\Customer::factory(300)->create();
 
+    //中間テーブルを介する多対多のテーブルにレコードを挿入する場合
     $items = Item::all();
-    Purchase::factory(100)->create()
+    Purchase::factory(10000)->create()
       ->each(function (Purchase $purchase) use ($items) {
         // ->items():絞り込みを行う場合はメソッド呼び出し
         // attach():リレーションの中間テーブルへのレコード挿入
-        // item_purchaseテーブルへのitem_id,quanitityの挿入
+        // item_purchaseテーブルへのitem_id(外部キー指定により第一引数で設定),quanitity(追加データは連想配列)の挿入
         $purchase->items()->attach(
           // pluck(key):modelのkeyに対応する値をコレクションとして取得->toArray:配列に変換
           $items->random(rand(1, 3))->pluck('id')->toArray(),
